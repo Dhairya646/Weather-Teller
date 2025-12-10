e6f71334337a448fdd7f6cf949e4662e09990a1a
DHAIRYA <dhairyadubey567@gmail.com>
2025-12-10 18:10:43 +0530

    calling api and display city name with temperature

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 async function showWeather() {
-    let city=document.getElementById("city").value;
-    let apiKey="befbb2a1a804d021b6b78157a1aa1d0a";
+    let city = document.getElementById("city").value;
+    let apiKey = "ff3de2a420c14b94b0b105213251012";
 
-    let url=``;
+    let url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=yes`;
 
     let response = await fetch(url);
-    let data= await response.json();
+    let data = await response.json();
     console.log(data);
 
-    if(data.cod===200){
-        document.getElementById("result").innerHTML= `
-            <h2>${data.name}, ${data.sys.country}</h2>
-
+    // WeatherAPI returns "error" when city not found
+    if (response.ok && !data.error) {
+        document.getElementById("result").innerHTML = `
+            <h2>${data.location.name}, ${data.location.country}</h2>
+            <p>Temperature: ${data.current.temp_c}°C</p>
+            <p>Condition: ${data.current.condition.text}</p>
         `;
     } else {
-
-        document.getElementById("result").innerHTML=`
-        <p>not found</p>
+        document.getElementById("result").innerHTML = `
+            <p>City not found</p>
         `;
     }
 }
